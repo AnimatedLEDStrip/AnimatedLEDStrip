9a5de187772940bef2d175a679cef6bd8b4fa8c4
Max Narvaez <narvae1@stolaf.edu>
2019-02-01 22:24:31 -0600

    Add license to new files

--- src/main/java/animatedledstrip/leds/LEDStripInterface.kt
+++ src/main/java/animatedledstrip/leds/LEDStripInterface.kt
@@ -1,5 +1,28 @@
 package animatedledstrip.leds
 
+/*
+ *  Copyright (c) 2019 AnimatedLEDStrip
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in
+ *  all copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *  THE SOFTWARE.
+ */
+
+
 interface LEDStripInterface {
 
     fun close()
@@ -10,7 +33,7 @@ interface LEDStripInterface {
 
     fun getPixelColor(pixel: Int): Int
 
-    fun setPixelColor(pixel: Int, color: Int): Unit
+    fun setPixelColor(pixel: Int, color: Int)
 
     fun setPixelColorRGB(pixel: Int, red: Int, green: Int, blue: Int) {
         setPixelColor(pixel, (red shl 16) or (green shl 8) or blue)
