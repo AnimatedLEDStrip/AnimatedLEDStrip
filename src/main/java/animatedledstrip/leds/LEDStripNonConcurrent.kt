28ae0c080c69ee6e5239e77f2e07a551380804ed
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-02-04 18:09:48 +0000

    Deploy AnimatedLEDStrip/AnimatedLEDStrip to github.com/AnimatedLEDStrip/AnimatedLEDStrip.git:gh-pages

---
+++ src/main/java/animatedledstrip/leds/LEDStripNonConcurrent.kt
@@ -0,0 +1,273 @@
+package animatedledstrip.leds
+
+/*
+ *  Copyright (c) 2019 AnimatedLEDStrip
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in
+ *  all copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *  THE SOFTWARE.
+ */
+
+
+import org.pmw.tinylog.Logger
+
+
+/**
+ * Class that represents an LED strip.
+ *
+ * @param numLEDs Number of LEDs in the strip
+ */
+abstract class LEDStripNonConcurrent(var numLEDs: Int): LEDStripSectionInterface {
+
+    /**
+     * The LED Strip. Chooses between `WS281x` and `EmulatedWS281x` based on value of emulated.
+     */
+    abstract var ledStrip : LEDStripInterface
+
+    init {
+        Logger.info("numLEDs: $numLEDs")
+    }
+
+    /**
+     * Sets a pixel's color with a `ColorContainer`.
+     *
+     * @param pixel The pixel to change
+     * @param colorValues The color to set the pixel to
+     */
+    open fun setPixelColor(pixel: Int, colorValues: ColorContainer) {
+        ledStrip.setPixelColorRGB(pixel, colorValues.r, colorValues.g, colorValues.b)
+    }
+
+
+    /**
+     * Set a pixel's color with `r`, `g`, `b` (ranges 0-255).
+     *
+     * @param pixel The pixel to change
+     * @param rIn Red intensity of the color
+     * @param gIn Green intensity of the color
+     * @param bIn Blue intensity of the color
+     */
+    fun setPixelColor(pixel: Int, rIn: Int, gIn: Int, bIn: Int) {
+        setPixelColor(pixel, ColorContainer(rIn, gIn, bIn))
+    }
+
+
+    /**
+     * Set a pixel's color with a `Long`, such as a 24-bit integer.
+     *
+     * @param pixel The pixel to change
+     * @param hexIn The color to set the pixel to
+     */
+    fun setPixelColor(pixel: Int, hexIn: Long) {
+        setPixelColor(pixel, ColorContainer(hexIn))
+    }
+
+    operator fun set(vararg pixels: Int, color: Long) {
+        for (pixel in pixels) {
+            setPixelColor(pixel, color)
+        }
+    }
+
+    operator fun set(pixels: IntRange, color: Long) {
+        for (pixel in pixels) {
+            setPixelColor(pixel, color)
+        }
+    }
+
+    operator fun set(vararg pixels: Int, color: ColorContainer) {
+        for (pixel in pixels) {
+            setPixelColor(pixel, color)
+        }
+    }
+
+    operator fun set(pixels: IntRange, color: ColorContainer) {
+        for (pixel in pixels) {
+            setPixelColor(pixel, color)
+        }
+    }
+
+    /**
+     * Set the color of the whole strip.
+     */
+    var color: Any?
+        get() = throw Exception("Cannot get color of whole strip")
+        set(value) {
+            when (value) {
+                is ColorContainer -> setStripColor(value)
+                is Long -> setStripColor(value)
+                is Int -> setStripColor(value.toLong())
+                else -> throw Exception("Invalid type")
+            }
+        }
+
+    /**
+     * Loops through all pixels and sets their color to `colorValues`.
+     *
+     * @param colorValues The color to set the strip to
+     */
+    fun setStripColor(colorValues: ColorContainer) {
+        for (i in 0 until numLEDs) setPixelColor(i, colorValues)
+        show()
+    }
+
+    /**
+     * Set the strip color with a `Long`, such as a 24-bit integer.
+     *
+     * @param hexIn The color to set the strip to
+     */
+    fun setStripColor(hexIn: Long) {
+        for (i in 0 until numLEDs) setPixelColor(i, hexIn)
+        show()
+    }
+
+
+    /**
+     * Set the strip color with `r`, `g`, `b` (ranges 0-255).
+     *
+     * @param rIn Red intensity of the color
+     * @param gIn Green intensity of the color
+     * @param bIn Blue intensity of the color
+     */
+    fun setStripColor(rIn: Int, gIn: Int, bIn: Int) {
+        for (i in 0 until numLEDs) setPixelColor(i, rIn, gIn, bIn)
+        show()
+    }
+
+    /**
+     * Set the color of a section of the strip. Loops through all LEDs between start
+     * and end (inclusive) and sets their color to `colorValues`.
+     *
+     * @param start First pixel in section
+     * @param end Last pixel in section
+     * @param colorValues The color to set the section to
+     */
+    override fun setSectionColor(start: Int, end: Int, colorValues: ColorContainer) {
+        for (i in start..end) setPixelColor(i, colorValues.r, colorValues.g, colorValues.b)
+        show()
+    }
+
+
+    /**
+     * Set a section's color with a `Long`, such as a 24-bit integer.
+     *
+     * @param start First pixel in section
+     * @param end Last pixel in section
+     * @param hexIn The color to set the section to
+     */
+    fun setSectionColor(start: Int, end: Int, hexIn: Long) {
+        for (i in start..end) setPixelColor(i, hexIn)
+        show()
+    }
+
+
+    /**
+     * Set a section's color with `r`, `g`, `b` (ranges 0-255).
+     *
+     * @param start First pixel in section
+     * @param end Last pixel in section
+     * @param rIn Red intensity of the color
+     * @param gIn Green intensity of the color
+     * @param bIn Blue intensity of the color
+     */
+    fun setSectionColor(start: Int, end: Int, rIn: Int, gIn: Int, bIn: Int) {
+        for (i in start..end) ledStrip.setPixelColorRGB(i, rIn, gIn, bIn)
+        show()
+    }
+
+
+    /**
+     * Get the color of a pixel.
+     *
+     * @param pixel The pixel to find the color of
+     * @return The color of the pixel
+     */
+    open fun getPixelColor(pixel: Int): ColorContainer =
+        ColorContainer(ledStrip.getPixelColor(pixel).toLong())
+
+
+    /**
+     * Use index operator for getPixelColor operations.
+     *
+     * @param pixel The pixel to find the color of
+     */
+    operator fun get(pixel: Int) = getPixelColor(pixel)
+
+    /**
+     * Get the color of a pixel as a `Long`.
+     *
+     * @param pixel The pixel to find the color of
+     * @return The color of the pixel as a Long
+     */
+    fun getPixelLong(pixel: Int): Long {
+        return getPixelColor(pixel).hex
+    }
+
+
+    /**
+     * Get the color of a pixel as a hexadecimal string.
+     *
+     * @param pixel The pixel to find the color of
+     * @return A `String` containing the color of the pixel in hexadecimal
+     */
+    fun getPixelHexString(pixel: Int): String {
+        return getPixelLong(pixel).toString(16)
+    }
+
+    /**
+     * Get the colors of all pixels as a `List<Long>`. Waits until each pixel's
+     * `Mutex` is unlocked.
+     */
+    val pixelColorList: List<Long>
+        get() {
+            val temp = mutableListOf<Long>()
+            for (i in 0 until numLEDs) temp.add(getPixelLong(i))
+            return temp
+        }
+
+    /**
+     * Set the color of the strip using a map with each pixel index mapped to a
+     * `ColorContainer`.
+     *
+     * @param palette The map of colors
+     * @param offset The index of the pixel that will be set to the color at
+     * index 0
+     */
+    fun setStripColorWithPalette(palette: Map<Int, ColorContainer>, offset: Int = 0) =
+        palette.forEach { i, j ->
+            setPixelColor((i + offset) % numLEDs, j)
+        }
+
+
+    /**
+     * Sets the color of the strip with a `List<ColorContainer>`. The list is converted to a map
+     * before that map is sent to [setStripColorWithPalette] with an offset of 0.
+     *
+     * @param colorList The list of colors
+     */
+    fun setStripColorWithGradient(colorList: List<ColorContainer>) {
+        val palette = colorsFromPalette(colorList, numLEDs)
+        setStripColorWithPalette(palette)
+    }
+
+
+    /**
+     * Send data to the LEDs.
+     */
+    open fun show() {
+        ledStrip.render()
+    }
+}
---
+++ src/test/java/AnimatedLEDStripTest.kt
@@ -0,0 +1,87 @@
+package animatedledstrip.test
+
+/*
+ *  Copyright (c) 2019 AnimatedLEDStrip
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in
+ *  all copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *  THE SOFTWARE.
+ */
+
+
+import animatedledstrip.leds.Animation
+import animatedledstrip.leds.AnimationData
+import animatedledstrip.leds.EmulatedAnimatedLEDStrip
+import org.junit.Test
+import org.pmw.tinylog.Configurator
+import org.pmw.tinylog.Level
+
+
+class AnimatedLEDStripTest {
+
+    init {
+        Configurator.defaultConfig().level(Level.OFF).activate()
+    }
+
+    @Test
+    fun testColor() {
+        val testLEDs = EmulatedAnimatedLEDStrip(50)
+
+        testLEDs.run(AnimationData().animation(Animation.COLOR).color(0xFF))
+        checkAllPixels(testLEDs, 0xFF)
+    }
+
+    @Test
+    fun testBounceToColor() {
+        val testLEDs = EmulatedAnimatedLEDStrip(50)
+
+        testLEDs.run(AnimationData().animation(Animation.BOUNCETOCOLOR).color(0xFF))
+        checkAllPixels(testLEDs, 0xFF)
+    }
+
+    @Test
+    fun testMultiPixelRunToColor() {
+        val testLEDs = EmulatedAnimatedLEDStrip(50)
+
+        testLEDs.run(AnimationData().animation(Animation.MULTIPIXELRUNTOCOLOR).color(0xFF))
+        checkAllPixels(testLEDs, 0xFF)
+    }
+
+    @Test
+    fun testSparkleToColor() {
+        val testLEDs = EmulatedAnimatedLEDStrip(50)
+
+        testLEDs.run(AnimationData().animation(Animation.SPARKLETOCOLOR).color(0xFF))
+        checkAllPixels(testLEDs, 0xFF)
+    }
+
+    @Test
+    fun testStack() {
+        val testLEDs = EmulatedAnimatedLEDStrip(50)
+
+        testLEDs.run(AnimationData().animation(Animation.STACK).color(0xFF))
+        checkAllPixels(testLEDs, 0xFF)
+    }
+
+    @Test
+    fun testWipe() {
+        val testLEDs = EmulatedAnimatedLEDStrip(50)
+
+        testLEDs.run(AnimationData().animation(Animation.WIPE).color(0xFF))
+        checkAllPixels(testLEDs, 0xFF)
+    }
+}
