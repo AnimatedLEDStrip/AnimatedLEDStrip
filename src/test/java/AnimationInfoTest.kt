35fa673d5ba5730b6fa4750c9871f9ba4feb8758
Max Narvaez <mnmax.narvaez3@gmail.com>
2019-07-09 19:57:49 -0500

    Remove color0-4 and add pCols to AnimationData

--- src/test/java/AnimationInfoTest.kt
+++ src/test/java/AnimationInfoTest.kt
@@ -12,11 +12,9 @@ class AnimationInfoTest {
         val ai = AnimationInfo()
 
         assertTrue { ai.abbr == "" }
-        assertTrue { ai.color1 == ReqLevel.NOTUSED }
-        assertTrue { ai.color2 == ReqLevel.NOTUSED }
-        assertTrue { ai.color3 == ReqLevel.NOTUSED }
-        assertTrue { ai.color4 == ReqLevel.NOTUSED }
-        assertTrue { ai.color5 == ReqLevel.NOTUSED }
+        assertTrue { ai.numReqColors == 0 }
+        assertTrue { ai.numOptColors == 0 }
+        assertTrue { ai.numColors == 0 }
         assertTrue { ai.delay == ReqLevel.NOTUSED }
         assertTrue { ai.delayDefault == 0L }
         assertTrue { ai.direction == ReqLevel.NOTUSED }
