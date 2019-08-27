eb58bd0e9d4759fd2d8e45bf466dda553c8f9650
Max Narvaez <mnmax.narvaez3@gmail.com>
2019-08-26 23:27:27 -0500

    Add copyright

--- src/main/java/animatedledstrip/colors/CCUtils.kt
+++ src/main/java/animatedledstrip/colors/CCUtils.kt
@@ -1,9 +1,32 @@
 package animatedledstrip.colors
 
+/*
+ *  Copyright (c) 2019 AnimatedLEDStrip
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in
+ *  all copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *  THE SOFTWARE.
+ */
+
+
 import animatedledstrip.utils.grayscale
 
 /**
- * Replaces all colors in [colors] with a grayscaled version of themselves.
+ * Replaces all colors in [ColorContainer.colors] with a grayscaled version of themselves.
  *
  * @return This ColorContainer instance
  */
@@ -84,7 +107,7 @@ fun ColorContainer.grayscaled(vararg indices: IntRange): ColorContainer {
 }
 
 /**
- * Replaces all colors in [colors] with their inverse.
+ * Replaces all colors in [ColorContainer.colors] with their inverse.
  *
  *@return This ColorContainer instance
  */
