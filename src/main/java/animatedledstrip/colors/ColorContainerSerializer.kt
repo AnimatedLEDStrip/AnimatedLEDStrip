40e2dab5321d2aef6ba0af42984f1e53a359933c
Max Narvaez <mnmax.narvaez3@gmail.com>
2019-10-26 17:39:11 -0500

    Remove nullability of serialization context and therefore the need for a null check

--- src/main/java/animatedledstrip/colors/ColorContainerSerializer.kt
+++ src/main/java/animatedledstrip/colors/ColorContainerSerializer.kt
@@ -4,14 +4,16 @@ import com.google.gson.*
 import java.lang.reflect.Type
 
 class ColorContainerSerializer : JsonSerializer<ColorContainerInterface>, JsonDeserializer<ColorContainerInterface> {
-    override fun serialize(color: ColorContainerInterface?, p1: Type?, context: JsonSerializationContext?): JsonElement {
-        requireNotNull(context)
-        return context.serialize(color as ColorContainer)
-    }
+    override fun serialize(
+        color: ColorContainerInterface,
+        p1: Type?,
+        context: JsonSerializationContext
+    ): JsonElement = context.serialize(color as ColorContainer)
 
-    override fun deserialize(json: JsonElement?, p1: Type?, context: JsonDeserializationContext?): ColorContainerInterface {
-        requireNotNull(context)
-        return context.deserialize(json, ColorContainer::class.java)
-    }
+    override fun deserialize(
+        json: JsonElement,
+        p1: Type?,
+        context: JsonDeserializationContext
+    ): ColorContainerInterface = context.deserialize(json, ColorContainer::class.java)
 
 }
