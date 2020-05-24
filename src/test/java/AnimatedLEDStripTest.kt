5dab45d0a5205834d7bd5e9c38b30099fed91e0d
Max Narvaez <mnmax.narvaez3@gmail.com>
2020-05-23 22:25:14 -0500

    Fix callback test

--- src/test/java/AnimatedLEDStripTest.kt
+++ src/test/java/AnimatedLEDStripTest.kt
@@ -131,8 +131,8 @@ class AnimatedLEDStripTest {
     fun testCallbacks() = runBlocking {
         var indicator1 = false
         var indicator2 = false
-        awaitPredefinedAnimationsLoaded()
         val testLEDs = EmulatedAnimatedLEDStrip(50)
+        awaitPredefinedAnimationsLoaded()
 
         testLEDs.startAnimationCallback = {
             indicator1 = true
