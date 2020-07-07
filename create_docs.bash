ab0680bc31ffe8ed4b7e530382ebbe053902ccbe
Max Narvaez <mnmax.narvaez3@gmail.com>
2020-07-07 13:22:26 -0500

    Test installing kotlin compiler before deploy step

--- create_docs.bash
+++ create_docs.bash
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-curl -s https://get.sdkman.io | bash
-
-sdk install kotlin
-
 git clone https://github.com/AnimatedLEDStrip/AnimatedLEDStrip.wiki.git wiki
 
 VERSION=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout 2> /dev/null)
