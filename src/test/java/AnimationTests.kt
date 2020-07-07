df0e73b58ca9caf9d0cc05af497301c5c4bcad1b
Max Narvaez <mnmax.narvaez3@gmail.com>
2020-07-07 15:55:00 -0500

    Add copyright to new files

--- src/test/java/AnimationTests.kt
+++ src/test/java/AnimationTests.kt
@@ -1,5 +1,5 @@
 /*
- *  Copyright (c) 2019 AnimatedLEDStrip
+ *  Copyright (c) 2019-2020 AnimatedLEDStrip
  *
  *  Permission is hereby granted, free of charge, to any person obtaining a copy
  *  of this software and associated documentation files (the "Software"), to deal
