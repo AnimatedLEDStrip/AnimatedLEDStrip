775b7615b3221d2896f3caf731c4503d14fcb258
Max Narvaez <mnmax.narvaez3@gmail.com>
2021-03-23 14:45:20 -0500

    1.0.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -46,7 +46,7 @@ repositories {
 }
 
 group = "io.github.animatedledstrip"
-version = "1.0.0-pre6.1"
+version = "1.0.0"
 description = "A library designed to simplify running animations on WS281x strips"
 
 kotlin {
