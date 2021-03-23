01a90e1a21be20961cf4d0dc69d26f79f5c84c29
Max Narvaez <mnmax.narvaez3@gmail.com>
2021-03-23 14:37:12 -0500

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -79,7 +79,7 @@ kotlin {
         val commonMain by getting {
             dependencies {
                 implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.1.0")
-                api("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.2")
+                api("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.3")
                 api("co.touchlab:kermit:0.1.8")
             }
         }
